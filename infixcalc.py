b4a5bdd4e725ccba8ad6487d0ac002a09d306580
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-10 20:52:55 -0300

    updt infixcalc.py

--- infixcalc.py
+++ infixcalc.py
@@ -33,18 +33,45 @@ import sys
 
 arguments = sys.argv[1:]
 
-op, a, b = arguments
-a = int(a)
-b = int(b)
+if not arguments:
+    operation = input("operacao:")
+    n1 = input("n1:")
+    n2 = input("n2:")
+    arguments = [operation, n1, n2]
 
-if op == "sum":
-    print(a + b)
-elif op == "sub":
-    print(a - b)
-elif op == "mul":
-    print(a * b)
-elif op == "div":
-    print(a / b)
+elif len(arguments) != 3:
+    print("Numero de argumentos invalidos")
+    print("ex: 'sum 5 5'")
+    sys.exit(1)
 
+operation, *nums = arguments
 
+valid_operations = ("sum", "sub", "mul", "div")
+if operation not in valid_operations:
+    print("Operacao invalida")
+    print(valid_operations)
+    sys.exit(1)
 
+validated_nums = []
+for num in nums:
+    if not num.replace(".",",").isdigit():
+        print(f"Numero invalido {num}")
+        sys.exit(1)
+    if "." in num:
+        num = float(num)
+    else:
+        num = int(num)
+    validated_nums.append(num)
+
+n1, n2 = validated_nums
+
+if operation == "sum":
+    result = n1 + n2
+elif operation == "sub":
+    result = n1 - n2
+elif operation == "mul":
+    result = n1 * n2
+elif operation == "div":
+    result = n1 / n2
+
+print(f"O resultado e {result}")
