9a19c2119897e0d8edd87b74d880f17ca9b54413
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-10 20:19:42 -0300

    updt hello.py

--- hello.py
+++ hello.py
@@ -10,6 +10,10 @@ Tenha a variavel LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe atraves do CLI argument '--lang'
+
+Ou o usuario tera que digitar.
+
 Execucao:
 
     python3 hello.py
@@ -21,8 +25,34 @@ __author__ = "Roger Ryuichi"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG","en_US.utf8")[:5]  # snake case
+
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")  # retorna lista com key na pos 0 e value na pos 1
+    key = key.lstrip("-").strip()  # lstrip pega todos os - do lado esquerda e remove
+    value = value.strip()
+    if key not in arguments:  
+        print(f"Invalid Option '{key}'")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    current_language = os.getenv("LANG")  # snake case
+    # TODO: Usar repeticao
+    if current_language is None:
+        current_language = input(
+            "Choose a language:"
+        )
+
+current_language = current_language[:5]
       
 msg = {
     "en_US": "Hello, World!",
@@ -33,4 +63,4 @@ msg = {
 }
 
 # O(1) - Constante - "IN"
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
