9b942fc3eaa5a1d6d38ea2dc7f45ff3f40cf3102
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-11 10:16:55 -0300

    updt infixcalc.py with logs

--- infixcalc.py
+++ infixcalc.py
@@ -24,11 +24,14 @@ n1: 5
 n2: 4
 9
 
+Os resultados serao salvos em 'infixcalc.log'
+
 """
 __version__ = "0.1.0"
 
 import os
 import sys
+from datetime import datetime
 
 
 arguments = sys.argv[1:]
@@ -73,5 +76,13 @@ elif operation == "mul":
     result = n1 * n2
 elif operation == "div":
     result = n1 / n2
+
+path = os.curdir
+filepath = os.path.join(path, "infixcalc.log")
+timestamp = datetime.now().isoformat()
+user = os.getenv('USER', 'anonymous')
+
+with open(filepath, 'a') as file_:
+    file_.write(f"{timestamp} - {user} - {operation},{n1},{n2} = {result}\n")
 
 print(f"O resultado e {result}")
